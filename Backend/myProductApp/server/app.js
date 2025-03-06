288aba290acd38146f0703a649bb7561bdd11e34
TanishaBhatt <hdfdash@gmail.com>
2025-03-06 12:13:07 +0530

    06.03.25

--- Backend/myProductApp/server/app.js
+++ Backend/myProductApp/server/app.js
@@ -1,110 +1,90 @@
-const express = require("express")
-const app = express()
-const port = 3002
-const user = []
+const express = require("express");
+const fs = require("fs/promises")
+const app = express();
+const port = 3002;
+let users = [];
 
-const m1 = (req, res, next) => {
-    const age = req.query.age;
-    if (!age) {
-        res.status(400).send("enter age in query")
+const loadUsers = async () => {
+    try {
+        const userdata = await fs.readFile("./users.json", "utf-8");
+        users = JSON.parse(userdata);
     }
-    else {
-        if (age < 18) {
-            res.status(400).send("user not authourised")
-        }
-        else {
-            next();
-        }
+    catch (err) {
+        users = [];
     }
 }
 
-//app.use(m1)
-app.use(express.json());
+const saveUser = async () => {
+    await fs.writeFile("./users.json", JSON.stringify(users));
+}
 
-app.get("/users", (req, res) => {
-    res.status(200).json(user)
+app.use(express.json())
+loadUsers();
+
+app.get("/users", async (req, res) => {
+    res.status(200).json(users);
 })
 
-app.get("user/:id", (req, res) => {
+app.get("/user/:id", (req, res) => {
     const uid = req.params.id;
-    const index = users.findIndex(ind => ind.id == uid)
+    const index = users.findIndex(ind => ind.id == uid);
     if (index == -1) {
-        res.status(400).json({
-            status: "fail",
-            message: "user not found"
-        })
+        res.status(400).json({ status: "fail", message: "User not found" });
     }
     else {
-        res.status(200).json({
-            status: "success",
-            message: "user found",
-            data: users[index]
-        })
+        res.status(200).json({ status: "success", message: "User found", data: users[index] });
     }
 })
 
-app.post("/createuser", (req, res) => {
-    const {name, email} = req.body
-    const newId = Date.now();
-    const newUser = {
-        id: newId, name, email
+app.post("/createuser", async (req, res) => {
+    const { name, email } = req.body;
+    if (!name || !email) {
+        res.status(400).json({ status: "fail", message: "name and email required to create user" })
     }
-    user.push(newUser)
-    res.status(201).json({
-        status: "success",
-        message: "user created successfully",
-        data: newUser
-    })
+    else {
+        const newId = Date.now();
+        const newUser = {
+            id: newId, name, email
+        }
+        users.push(newUser);
+        saveUser();
+        res.status(201).json({ status: "success", message: "user created Successfully", data: newUser })
+    }
 })
 
 app.patch("/edituser/:id", (req, res) => {
-    const uid = req.params.id
-    const {name, email} = req.body
+    const uid = req.params.id;
+    const { name, email } = req.body;
     if (!name || !email) {
-        res.status(400).json({
-            status: "fail",
-            message: "all fields required"
-        })
+        res.status(400).json({ status: "fail", message: "All Fields Required" })
     }
     else {
-        const index = user.findIndex(ind => ind.id == uid)
+        const index = users.findIndex(ind => ind.id == uid);
         if (index == -1) {
-            res.status(400).json({
-                status: "fail",
-                message: "user not found"
-            })
+            res.status(400).json({ status: "fail", message: "User not found" });
         }
         else {
-            user[index].name = name
-            user[index].email = email
-            res.status(200).json({
-                status: "success",
-                message: "user found",
-                data: users[index]
-            })
+            users[index].name = name;
+            users[index].email = email;
+            saveUser();
+            res.status(200).json({ status: "success", message: "user Edited Successfully", data: users[index] })
         }
     }
 })
 
 app.delete("/deleteuser/:id", (req, res) => {
     const uid = req.params.id;
-    const index = user.findIndex(ind => ind.id == uid)
+    const index = users.findIndex(ind => ind.id == uid)
     if (index == -1) {
-        res.status(400).json({
-            status: "fail",
-            message: "user not found"
-        })
+        res.status(400).json({ status: "fail", message: "User not found" });
     }
     else {
-        const deldata = users.slpice(index, 1)
-        res.status(200).json({
-            status: "success",
-            message: "user deleted",
-            data: deldata
-        })
+        const deldata = users.splice(index, 1);
+        saveUser();
+        res.status(200).json({ status: "success", message: "User Deleted Successfully", data: deldata })
     }
 })
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+    console.log(`Server is Running on port ${port}`)
 })
